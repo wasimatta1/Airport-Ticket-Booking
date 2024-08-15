41fbdae4eff3256b5d0599dfc9ba517d7ae837b8
wasimatta1 <wasim.atta33@hotmail.com>
2024-08-15 15:38:33 +0300

    Create Filter Method and Finish Passenger Menu

--- Airport Ticket Booking/Domin/Extension/ExtensionMethod.cs
+++ Airport Ticket Booking/Domin/Extension/ExtensionMethod.cs
@@ -1,26 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq.Expressions;
+
 
 namespace Airport_Ticket_Booking.Domin.Extension
 {
     public static class ExtensionMethod
     {
-        public static IEnumerable<T> Filter<T>(this IEnumerable<T> input, Dictionary<string,object> filters)
+        public static IEnumerable<T>? Filter<T>(this IEnumerable<T> input, Dictionary<string, (FilterOperator, object)> filters)
         {
-            if (input == null)
+            if (input is null || filters is null || filters.Count == 0)
                 return null;
 
-            //TODO: ..
-            //The Filter method will filter the input list based on the conditions
-            // will used Expressions to build the conditions
-            // and use reflection to get the property value of the object
-            // and compare it with the value in the filter using our custom expression<Func>
+            foreach (var filter in filters)
+            {
+                var filterOperator = filter.Value.Item1;
+                var filterValue = filter.Value.Item2;
+                ConstantExpression value = Expression.Constant(filterValue);
+                ParameterExpression parameter = Expression.Parameter(typeof(T), "x");
+                MemberExpression member = Expression.Property(parameter, filter.Key);
+                Expression? condition = null;
+                switch (filterOperator)
+                {
+                    case FilterOperator.Equals:
+                        condition = Expression.Equal(member, value);
+                        break;
+                    case FilterOperator.GreaterThan:
+                        condition = Expression.GreaterThan(member, value);
+                        break;
+                    case FilterOperator.LessThan:
+                        condition = Expression.LessThan(member, value);
+                        break;
+                    case FilterOperator.GreaterThanOrEqual:
+                        condition = Expression.GreaterThanOrEqual(member, value);
+                        break;
+                    case FilterOperator.LessThanOrEqual:
+                        condition = Expression.LessThanOrEqual(member, value);
+                        break;
+                    case FilterOperator.NotEqual:
+                        condition = Expression.NotEqual(member, value);
+                        break;
+                }
 
+                var lambda = Expression.Lambda<Func<T, bool>>(condition!, new ParameterExpression[] { parameter });
+                input = input.Where(lambda.Compile());
+            }
             return input;
         }
+
         public static void Print<T>(this IEnumerable<T> source, string title)
         {
             if (source == null)
@@ -38,6 +63,16 @@ namespace Airport_Ticket_Booking.Domin.Extension
 
             }
         }
+        public enum FilterOperator
+        {
+            Equals,
+            GreaterThan,
+            LessThan,
+            GreaterThanOrEqual,
+            LessThanOrEqual,
+            NotEqual
+        }
+
 
     }
 }
--- Airport Ticket Booking/Program.cs
+++ Airport Ticket Booking/Program.cs
@@ -1,20 +1,19 @@
 ﻿using Airport_Ticket_Booking.Domin.Base;
 using Airport_Ticket_Booking.Domin.CSV;
+using Airport_Ticket_Booking.Domin.Extension;
 using Airport_Ticket_Booking.Domin.Users;
+using static Airport_Ticket_Booking.Domin.Extension.ExtensionMethod;
 
 namespace Airport_Ticket_Booking
 {
     internal class Program
     {
-        static void Main(string[] args)
-        {   // TODO ...
-            // all code until now just to test , it will be updated soon 
-            //string flightsFilePath = "C:\\Users\\wasim\\OneDrive\\Desktop\\C# project\\Airport Ticket Booking\\Flights.csv";
-            //var flights = CSVRepo.LoadDataFromCSVFile<Flight>(flightsFilePath);
-            //flights.Print("Flights");
+        static string FilePath = @"C:\Users\wasim\OneDrive\Desktop\C# project\Airport Ticket Booking\";
 
-            string userFilePath = "C:\\Users\\wasim\\OneDrive\\Desktop\\C# project\\Airport Ticket Booking\\Users.csv";
-            var Users = CSVRepo.LoadDataFromCSVFile<User>(userFilePath);
+        static List<Flight>? flights;
+        static void Main(string[] args)
+        { 
+            var Users = CSVRepo.LoadDataFromCSVFile<User>($"{FilePath}Users.csv");
             User? user;
             while (true)
             {
@@ -46,11 +45,13 @@ namespace Airport_Ticket_Booking
         }
         static void PassengerMenu(Passenger passenger)
         {
-            string BookingsFilePath = "C:\\Users\\wasim\\OneDrive\\Desktop\\C# project\\Airport Ticket Booking\\Bookings.csv";
+            flights = CSVRepo.LoadDataFromCSVFile<Flight>($"{FilePath}Flights.csv");
 
-            List<Booking> bookings = CSVRepo.LoadDataFromCSVFile<Booking>(BookingsFilePath);
+            List<Booking> bookings = CSVRepo.LoadDataFromCSVFile<Booking>($"{FilePath}Bookings.csv");
 
             passenger.Bookings = bookings.Where(x => x.PassengerUserName == passenger.UserName).ToList();
+
+            IEnumerable<Flight>? filteredFlights = flights; // Default value All Flights
 
             while (true)
             {
@@ -64,21 +65,32 @@ namespace Airport_Ticket_Booking
                 Console.Write("Please select an option (1-7): ");
 
                 string? input = Console.ReadLine();
-
                 switch (input)
                 {
                     case "1":
-                        //TODo...
+                        Booking? bookingFlight = BookingNewFlight(filteredFlights,passenger);
+                        if(bookingFlight is null)
+                            break;
+                        passenger.Bookings.Add(bookingFlight);
+                        bookings.Add(bookingFlight);
+                        CSVRepo.SaveDataToCSVFile($"{FilePath}Bookings.csv", bookings);
                         break;
                     case "2":
-                        //TODo...
+
+                        var filters = GetFilters(false);
+                        if (filters.Count == 0)
+                            filteredFlights = flights;
+                        else 
+                            filteredFlights = flights.Filter(filters);
+
+                        filteredFlights?.Print("Available Flights");
                         break;
                     case "3":
                         Booking? removedBooking;
-                        if (CancelBooking(passenger,out removedBooking))
-                        {   
+                        if (CancelBooking(passenger, out removedBooking))
+                        {
                             bookings.Remove(removedBooking!);
-                            CSVRepo.SaveDataToCSVFile(BookingsFilePath, bookings);
+                            CSVRepo.SaveDataToCSVFile($"{FilePath}Bookings.csv", bookings);
                         }
                         break;
                     case "4":
@@ -87,14 +99,14 @@ namespace Airport_Ticket_Booking
                         {
                             var preUpdateBook = bookings.Single(x => x.BookingId == modifiedBooking!.BookingId);
                             preUpdateBook = modifiedBooking!;
-                            CSVRepo.SaveDataToCSVFile(BookingsFilePath, bookings);
+                            CSVRepo.SaveDataToCSVFile($"{FilePath}Bookings.csv", bookings);
                         }
                         break;
                     case "5":
                         passenger.Bookings.Print("Your Bookings");
                         break;
                     case "6":
-                        return; 
+                        return;
                     default:
                         Console.WriteLine("Invalid option, please try again.");
                         break;
@@ -103,6 +115,204 @@ namespace Airport_Ticket_Booking
                 Console.WriteLine();
             }
         }
+
+        private static Booking? BookingNewFlight(IEnumerable<Flight>? filteredFlights, Passenger passenger)
+        {
+            filteredFlights?.Print("Available Flights From Last Search");
+
+            Console.Write("Enter the Flight ID you want to book: ");
+
+            string? flightId = Console.ReadLine();
+
+            Flight? flight = filteredFlights?.SingleOrDefault(x => x.FlightId == flightId);
+
+            if (flight is null)
+            {
+                Console.WriteLine("Invalid Flight ID");
+               return null;
+            }
+            bool isBooked = passenger.Bookings.Any(x => x.FlightId == flightId);
+
+            if (isBooked)
+            {
+                Console.WriteLine("You already booked this flight");
+                return null;
+            }
+            Console.Write("Enter the Class {Economy,Business,First Class}: ");
+            string? flightClass = Console.ReadLine();
+
+
+            if (!Enum.IsDefined(typeof(Class), flightClass?.Replace(" ", "")!))
+            {
+                Console.WriteLine("Invalid Class");
+                return null;
+            }
+            var price = CalculatePrice("Economy", flightClass!, flight.Price);
+
+            var NewBook = new Booking
+            {
+                BookingId = Guid.NewGuid().ToString(),
+                PassengerUserName = passenger.UserName,
+                FlightId = flightId!,
+                Price = price,
+                DepartureCountry = flight.DepartureCountry,
+                DestinationCountry = flight.DestinationCountry,
+                DepartureDate = flight.DepartureDate,
+                DepartureAirport = flight.DepartureAirport,
+                ArrivalAirport = flight.ArrivalAirport,
+                Class = flightClass!
+            };
+
+            return NewBook;
+        }
+
+        private static Dictionary<string, (FilterOperator, object)> GetFilters(bool isManger)
+        {
+            var filters = new Dictionary<string, (FilterOperator, object)>();
+
+            while (true)
+            {
+                Console.WriteLine("Choose a parameter to filter by:");
+                Console.WriteLine("1- Price");
+                Console.WriteLine("2- Departure Country");
+                Console.WriteLine("3- Destination Country");
+                Console.WriteLine("4- Departure Date");
+                Console.WriteLine("5- Departure Airport");
+                Console.WriteLine("6- Arrival Airport");
+                if (isManger)
+                {
+                    Console.WriteLine("7- Flight ID");
+                    Console.WriteLine("8- Passenger UserName");
+                    Console.WriteLine("9- Finish");
+                }else
+                    Console.WriteLine("7- Finish");
+
+                string? choice = Console.ReadLine();
+
+                if(int.TryParse(choice, out int x))
+                {
+                    if (!isManger && x > 7 || x < 1)
+                    {
+                        Console.WriteLine("Invalid Choose selected, please try again.");
+                        continue;
+                    }else if (isManger && x > 9 || x < 1)
+                    {
+                        Console.WriteLine("Invalid Choose selected, please try again.");
+                        continue;
+                    }
+                }
+
+                if (!isManger && choice == "7" || isManger && choice == "9")
+                {
+                    break; // Finish
+                }
+
+                Console.WriteLine("Select the filter operation:");
+                Console.WriteLine("1- Equals");
+                Console.WriteLine("2- Not Equal");
+                var isVaildOpreator = choice == "4" || choice == "1";
+                if (isVaildOpreator)
+                {
+                    Console.WriteLine("3- Less Than");
+                    Console.WriteLine("4- Greater Than or Equal");
+                    Console.WriteLine("5- Less Than or Equal");
+                    Console.WriteLine("6- Greater Than");
+                }
+
+
+                string? operationChoice = Console.ReadLine();
+                if (!isVaildOpreator)
+                {
+                    switch (operationChoice)
+                    {
+                        case "2":
+                        case "3":
+                        case "5":
+                        case "6":
+                        case "7":
+                        case "8":
+                            Console.WriteLine("Invalid operation selected, please try again.");
+                            continue;
+                    }
+                }
+
+                FilterOperator filterOperator = FilterOperator.Equals; 
+
+                switch (operationChoice)
+                {
+                    case "1":
+                        filterOperator = FilterOperator.Equals;
+                        break;
+                    case "2":
+                        filterOperator = FilterOperator.NotEqual;
+                        break;
+                    case "3":
+                        filterOperator = FilterOperator.LessThan;
+                        break;
+                    case "4":
+                        filterOperator = FilterOperator.GreaterThanOrEqual;
+                        break;
+                    case "5":
+                        filterOperator = FilterOperator.LessThanOrEqual;
+                        break;
+                    case "6":
+                        filterOperator = FilterOperator.GreaterThan;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid operation selected, please try again.");
+                        continue;
+                }
+
+                Console.WriteLine("Enter the value for the selected parameter:");
+
+                string? filterValueStr = Console.ReadLine();
+                if (filterValueStr is null)
+                {
+                    Console.WriteLine("Invalid value entered, please try again.");
+                    continue;
+                }
+                object filterValue;
+
+                switch (choice)
+                {
+                    case "1":
+                        filterValue = double.Parse(filterValueStr);
+                        filters.Add("Price", (filterOperator, filterValue));
+                        break;
+                    case "2":
+                        filterValue = filterValueStr;
+                        filters.Add("DepartureCountry", (filterOperator, filterValue));
+                        break;
+                    case "3":
+                        filterValue = filterValueStr;
+                        filters.Add("DestinationCountry", (filterOperator, filterValue));
+                        break;
+                    case "4":
+                        filterValue = DateTime.Parse(filterValueStr);
+                        filters.Add("DepartureDate", (filterOperator, filterValue));
+                        break;
+                    case "5":
+                        filterValue = filterValueStr;
+                        filters.Add("DepartureAirport", (filterOperator, filterValue));
+                        break;
+                    case "6":
+                        filterValue = filterValueStr;
+                        filters.Add("ArrivalAirport", (filterOperator, filterValue));
+                        break;
+                    case "7":
+                        filterValue = filterValueStr;
+                        filters.Add("FlightId", (filterOperator, filterValue));
+                        break;
+                    case "8":
+                        filterValue = filterValueStr;
+                        filters.Add("PassengerUserName", (filterOperator, filterValue));
+                        break;
+                }
+            }
+
+            return filters;
+        }
+
         static bool CancelBooking(Passenger passenger,out Booking? removedBooking)
         {
             Console.Write("Enter the Booking ID you want to cancel: ");
@@ -158,6 +368,7 @@ namespace Airport_Ticket_Booking
             price *= (int)Enum.Parse(typeof(Class), newClass.Replace(" ", ""));
             return price;
         }
+
 
         enum Class
         {
